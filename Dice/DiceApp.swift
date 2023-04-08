a7c1ec363e2958b5b599c23b9f48b70caed187cf
ko-suke24 <kosuke0322@gmail.com>
2023-04-08 23:16:59 +0900

    first commit

---
+++ Dice/DiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceApp.swift
+//  Dice
+//
+//  Created by RURU.jr on 2023/04/08.
+//
+
+import SwiftUI
+
+@main
+struct DiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
